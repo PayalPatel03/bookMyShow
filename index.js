a220283958fc1b48597bb37396c81578ffdb5d99
PayalPatel03 <p6579113@gmail.com>
2025-08-19 16:28:47 +0530

    done

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
-const express=require('express');
-const app=express();
-const port=8081;
+const express = require('express');
+const app = express();
+const port = 8081;
+
 app.set('view engine', 'ejs'); 
 app.use(express.urlencoded({ extended: true }));
+
+
 app.get("/", (req, res) => {
-    res.render('index');   // looks for views/home.ejs
+    res.render("index"); 
 });
+
+
+app.get("/movie-details", (req, res) => {
+    res.render("movie-details");  
+});
+
 app.use(express.static("public"));
 
-app.listen(port,(err)=>{
-    if(err){
+app.listen(port, (err) => {
+    if (err) {
         console.log(err.message);
+    } else {
+        console.log("Server Started");
+        console.log("http://localhost:" + port);
     }
-    else{
-        console.log("Server Stared");
-        console.log("http://localhost:"+port);
-    }
-})
+});
