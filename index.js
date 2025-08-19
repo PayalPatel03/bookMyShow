278934558019eedbd1fdc61b8e2a32339dea4160
PayalPatel03 <p6579113@gmail.com>
2025-08-19 23:56:10 +0530

    donee

--- index.js
+++ index.js
@@ -1,27 +1,21 @@
 const express = require('express');
+const db = require('./configs/db');
+const path = require('path')
 const app = express();
-const port = 8081;
+const port = 1503;
 
 app.set('view engine', 'ejs'); 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('assets'))
+app.use('/uploads',express.static((__dirname,'uploads')))
 
-
-app.get("/", (req, res) => {
-    res.render("index"); 
-});
-
-
-app.get("/movie-details", (req, res) => {
-    res.render("movie-details");  
-});
-
-app.use(express.static("public"));
+app.use('/',require('./routers/movie'))
+app.use('/',require('./routers/trailers'))
 
 app.listen(port, (err) => {
-    if (err) {
-        console.log(err.message);
-    } else {
-        console.log("Server Started");
+    if (!err) {
+        db()
+        console.log("Server started...");
         console.log("http://localhost:" + port);
     }
 });
